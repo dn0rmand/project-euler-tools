1521a4983c5c30c449151d5374a06c3d095b3054
Dominique Normand <dnormand@us.imshealth.com>
2022-04-20 19:25:40 -0400

    - PreciseNumber now support both BigInt and Number. BigInt is the default. use `PreciseNumber.setUseBigInt(false)` to swith to Number

--- src/preciseNumber.js
+++ src/preciseNumber.js
@@ -1,208 +1,224 @@
+require('./numberHelper');
 require('./bigintHelper');
 
-class PreciseNumber
-{
-    static Infinity = PreciseNumber.create(Infinity, 1n);
-    static Zero     = PreciseNumber.create(0n, 1n);
-    static One      = PreciseNumber.create(1n, 1n);
+let useBigInt = true;
 
+function BIG(v, convertToNumber) {
+    if (v === Infinity) {
+        return v;
+    }
+    if (useBigInt) {
+        return BigInt(v);
+    } else if (convertToNumber) {
+        return Number(v);
+    }
+    return v;
+}
+
+class PreciseNumber {
     static allowSimplification = true;
 
-    static create(numerator, divisor) 
-    {
+    static setUseBigInt(value) {
+        useBigInt = value;
+
+        PreciseNumber.Infinity = PreciseNumber.create(Infinity, BIG(1));
+        PreciseNumber.Zero = PreciseNumber.create(BIG(0), BIG(1));
+        PreciseNumber.One = PreciseNumber.create(BIG(1), BIG(1));
+    }
+
+    static Infinity = PreciseNumber.create(Infinity, BIG(1));
+    static Zero = PreciseNumber.create(BIG(0), BIG(1));
+    static One = PreciseNumber.create(BIG(1), BIG(1));
+
+    static create(numerator, divisor) {
         const p = new PreciseNumber();
         if (numerator !== Infinity) {
-            p.numerator = BigInt(numerator);
-            p.divisor   = BigInt(divisor);
+            p.numerator = BIG(numerator, true);
+            p.divisor = BIG(divisor, true);
             p.simplify();
         } else {
-            p.numerator = numerator;
-            p.divisor   = 1n;
+            p.numerator = BIG(numerator, true);
+            p.divisor = BIG(1);
         }
         return p;
     }
 
-    constructor(value)
-    {
+    constructor(value) {
         if (value === undefined) {
-            this.numerator = 0n;
-            this.divisor = 1n;
+            this.numerator = BIG(0);
+            this.divisor = BIG(1);
         } else if (value === Infinity) {
             this.numerator = Infinity;
-            this.divisor   = 1n;
+            this.divisor = BIG(1);
         } else if (value instanceof PreciseNumber) {
             this.numerator = value.numerator;
-            this.divisor   = value.divisor;
-        } else if (typeof(value) === 'bigint') {
-            this.divisor = 1n;
-            this.numerator = value;
+            this.divisor = value.divisor;
+        } else if (typeof (value) === 'bigint') {
+            this.divisor = BIG(1);
+            this.numerator = BIG(value, true);
         } else {
             value = +value;
             if (isNaN(value)) {
                 throw "Not a valid number";
             }
-            this.divisor   = 1n;
+            this.divisor = BIG(1);
+            const TEN = BIG(10);
             while (Math.floor(value) !== value) {
-                this.divisor *= 10n;
+                this.divisor *= TEN;
                 value *= 10;
             }
-            this.numerator = BigInt(value);
+            this.numerator = value;
             this.simplify();
         }
     }
 
-    simplify()
-    {
+    simplify() {
         if (!PreciseNumber.allowSimplification) {
             return;
         }
 
-        if (this.divisor === 0n) {
-            this.numerator = 0n;
-            this.divisor   = 0n;
-            return; 
-        }
-        if (this.numerator === 0n) {
-            this.divisor   = 1n;
-            this.numerator = 0n;
+        if (!this.divisor) {
+            this.numerator = BIG(0);
+            this.divisor = BIG(1);
             return;
-        } 
-        if (this.divisor < 0n) {
+        }
+        if (!this.numerator) {
+            this.divisor = BIG(1);
+            this.numerator = BIG(0);
+            return;
+        }
+        if (this.divisor < 0) {
             // It's better is the negative value is the numerator
             this.numerator = -this.numerator;
-            this.divisor   = -this.divisor;
+            this.divisor = -this.divisor;
         }
 
         let g = this.numerator.gcd(this.divisor);
         if (g < 0) g = -g;
-        if (g !== 1n) {
+        if (g != 1) {
             this.numerator /= g;
-            this.divisor   /= g;
+            this.divisor /= g;
         }
     }
 
-    plus(other) 
-    {
-        if (! (other instanceof PreciseNumber)) {
+    plus(other) {
+        if (!(other instanceof PreciseNumber)) {
             other = new PreciseNumber(other);
         }
 
         const state = new PreciseNumber();
 
         state.numerator = this.numerator * other.divisor + this.divisor * other.numerator;
-        state.divisor   = this.divisor * other.divisor;
-        
+        state.divisor = this.divisor * other.divisor;
+
         state.simplify();
         return state;
     }
 
-    minus(other) 
-    {
-        if (! (other instanceof PreciseNumber)) {
+    minus(other) {
+        if (!(other instanceof PreciseNumber)) {
             other = new PreciseNumber(other);
         }
         const state = new PreciseNumber();
 
         state.numerator = this.numerator * other.divisor - this.divisor * other.numerator;
-        state.divisor   = this.divisor * other.divisor;
-        
+        state.divisor = this.divisor * other.divisor;
+
         state.simplify();
         return state;
     }
 
-    times(other)
-    {
-        if (! (other instanceof PreciseNumber)) {
+    times(other) {
+        if (!(other instanceof PreciseNumber)) {
             other = new PreciseNumber(other);
         }
         const state = new PreciseNumber();
 
-        state.divisor   = this.divisor * other.divisor;
+        state.divisor = this.divisor * other.divisor;
         state.numerator = this.numerator * other.numerator;
 
         state.simplify();
         return state;
     }
 
-    divide(other)
-    {
-        if (! (other instanceof PreciseNumber)) {
+    divide(other) {
+        if (!(other instanceof PreciseNumber)) {
             other = new PreciseNumber(other);
         }
         const state = new PreciseNumber();
 
-        state.divisor   = this.divisor * other.numerator;
+        state.divisor = this.divisor * other.numerator;
         state.numerator = this.numerator * other.divisor;
 
         state.simplify();
         return state;
     }
 
-    get valid() 
-    {
-        return this.divisor !== 0n;
+    get valid() {
+        return this.divisor;
     }
 
-    reverse() 
-    {
+    reverse() {
         const r = new PreciseNumber();
         r.divisor = this.numerator;
         r.numerator = this.divisor;
-        return r;    
+        return r;
     }
 
-    equals(other) 
-    {
-        if (! (other instanceof PreciseNumber)) {
+    equals(other) {
+        if (!(other instanceof PreciseNumber)) {
             other = new PreciseNumber(other);
         }
         return other.numerator === this.numerator && other.divisor === this.divisor;
     }
 
-    pow(value) 
-    {
+    pow(value) {
         let v = PreciseNumber.One;
 
-        for(let p = 1; p <= value; p++) {
+        for (let p = 1; p <= value; p++) {
             v = v.times(this);
         }
 
         return v;
     }
 
-    less(other)
-    {
-        if (this.equals(other)) { return false; }
+    less(other) {
+        if (this.equals(other)) {
+            return false;
+        }
 
         const diff = this.minus(other);
         return (diff.numerator < 0);
     }
 
-    greater(other)
-    {
-        if (this.equals(other)) { return false; }
+    greater(other) {
+        if (this.equals(other)) {
+            return false;
+        }
         return new PreciseNumber(other).less(this);
     }
 
     toString() {
-        if (this.divisor === 1n) {
+        if (this.divisor == 1) {
             return `${this.numerator}`;
         }
         return `${this.numerator}/${this.divisor}`;
     }
 
-    get isInfinity()
-    {
+    get isInfinity() {
         return this.numerator === Infinity;
     }
 
-    valueOf()
-    {
-        if (this.isInfinity && this.divisor === 0n) {
+    valueOf(precision) {
+        if (this.isInfinity && this.divisor == 0) {
             return Infinity;
         }
 
-        return this.numerator.divise(this.divisor, 10);
+        if (useBigInt) {
+            return this.numerator.divise(this.divisor, precision || 10);
+        } else {
+            return this.numerator / this.divisor;
+        }
     }
 }
 
